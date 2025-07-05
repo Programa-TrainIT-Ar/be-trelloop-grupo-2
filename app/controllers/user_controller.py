836d255ebdf292309a8823b2da5ee394c3961ddc
KevinVillca <kevtorricov@gmail.com>
2025-07-05 17:47:40 -0400

    Actualizacion de bases de datos y funciones de crear y obtener usuarios

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,9 +1,23 @@
 from app.models.user import User
 from app.database.database import db
-from flask import jsonify
+from flask import jsonify ,request
 from ..logs.logger import logger
 
 def get_users():
     users = User.query.all()
-    logger.info("Este es un mensaje de informacion")
-    return jsonify({"message": "Soy un usuario"})
+    return jsonify([user.to_dict() for user in users])
+
+def create_users():
+    data = request.json
+    user = User(
+        name = data["name"],
+        email = data["email"]
+    )
+    user.set_password(data["password"])
+    
+    db.session.add(user)
+    db.session.commit()
+    
+    logger.info(f"Usuario creado: {user.email}")
+
+    return jsonify({"message": "Usuario creado exitosamente"}), 201
