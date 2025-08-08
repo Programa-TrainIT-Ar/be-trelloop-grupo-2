5164f7172eb9a67f77c2c04b0c1fb894658f0ccd
Zagarsix <claudk1006@gmail.com>
2025-08-08 02:37:48 -0400

    feature: se agrega el endpoint para editar el nombre de una lista

--- app/routes/list_routes.py
+++ app/routes/list_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify
 from flask_jwt_extended import jwt_required
 from flasgger.utils import swag_from
-from ..controllers.list_controller import get_lists_by_board, create_list, delete_list
+from ..controllers.list_controller import get_lists_by_board, create_list, delete_list, update_list
 
 list_bp = Blueprint('list', __name__, url_prefix='/api/boards')
 
@@ -22,3 +22,8 @@ def handle_create_list(board_id):
 @jwt_required()
 def handle_delete_list(board_id, list_id):
     return delete_list(board_id, list_id)
+
+@list_bp.route('/<int:board_id>/lists/<int:list_id>', methods=['PUT'])
+@jwt_required()
+def handle_update_list(board_id, list_id):
+    return update_list(board_id, list_id)
