3d7d6dec3d63e2a5fdae4647286af028dbf5dea5
Zagarsix <claudk1006@gmail.com>
2025-08-20 00:56:59 -0400

    se agregan nuevos campos al modelo tag

--- app/models/tag.py
+++ app/models/tag.py
@@ -1,5 +1,4 @@
 from ..database.database import db
-from .relationships import board_tag
 
 class Tag(db.Model):
     """Tag model for labeling boards"""
@@ -8,11 +7,16 @@ class Tag(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
-    boards = db.relationship('Board', secondary=board_tag, back_populates='tags')
+    color = db.Column(db.String(20), nullable=False, default="#414141")
+    board_id = db.Column(db.Integer, db.ForeignKey('boards.id'), nullable=False)
+
+    __table_args__ = (db.UniqueConstraint('name', 'board_id', name='unique_tag_per_board'),)
 
     def to_dict(self):
         """Convert list to dictionary for API responses"""
         return {
             'id': self.id,
-            'name': self.name
+            'name': self.name,
+            'color': self.color,
+            'board_id': self.board_id
         } 
