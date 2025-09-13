0bab2695065075e03dd69e09593872ee4ec831b0
Zagarsix <claudk1006@gmail.com>
2025-09-13 15:43:32 -0300

    integración de socketIO

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ from .routes import all_blueprints
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
 from flasgger import Swagger
+from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
@@ -47,15 +48,22 @@ app.debug = True
 app.config["SQLALCHEMY_DATABASE_URI"] = config["DATABASE_URL"]
 app.config["JWT_SECRET_KEY"] = config["JWT_SECRET_KEY"]
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=config["JWT_ACCESS_TOKEN_EXPIRES"])
+socketio = SocketIO(
+     app, 
+     cors_allowed_origins=config["CORS_ORIGINS"],
+     async_mode="eventlet")
 CORS(app, origins=config["CORS_ORIGINS"])
 
 db.init_app(app)
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
 
+# Registrar eventos de websocket
+from .websocket.board_events import register_board_events
+register_board_events(socketio)
 
 for bp in all_blueprints:
     app.register_blueprint(bp)
 
 if __name__ == "__main__":
-     app.run(host="0.0.0.0", port=5000, debug=True)
+     socketio.run(app, host="0.0.0.0", port=5000, debug=True)
